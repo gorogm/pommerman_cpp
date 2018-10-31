bbe3f19320b1e057ace93fcddebe5086cb23e896
marton.gorog <marton.gorog@aimotive.com>
2018-10-31 21:59:02 +0100

    exploding several bombs in one step fixed

--- src/bboard/step_utility.cpp
+++ src/bboard/step_utility.cpp
@@ -122,7 +122,7 @@ void TickBombs(State& state)
         if(BMB_TIME(state.bombs[0]) == 0)
         {
             state.ExplodeTopBomb();
-            //i--;
+            i--;
         }
         else
         {
