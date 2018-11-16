99f9380f2e86758a572ee10657d7ce78169a7396
Marton Gorog <gorogm@gmail.com>
2018-11-16 17:36:12 +0100

    static function

--- src/bboard/bboard.hpp
+++ src/bboard/bboard.hpp
@@ -617,7 +617,7 @@ void StartGame(State* state, Agent* agents[AGENT_COUNT], int timeSteps);
  * @brief Prints the state into the standard output stream.
  * @param state The state to print
  */
-void PrintState(State* state);
+void PrintState(const State* state);
 
 /**
  * @brief Returns a string, corresponding to the given item
