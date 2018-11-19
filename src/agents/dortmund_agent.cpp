5f14d3290d71b47b23b470485af028f57e64ac9d
Marton Gorog <gorogm@gmail.com>
2018-11-19 22:17:52 +0100

    bit relaxed time constraints

--- src/agents/dortmund_agent.cpp
+++ src/agents/dortmund_agent.cpp
@@ -436,7 +436,9 @@ namespace agents {
                         {
 #ifdef TIME_LIMIT_ON
                             size_t millis = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count();
-                            if ((millis < 90) || (depth == 0 && millis < 95))
+                            if(millis > 97)
+                                std::cout << "OVERTIME " << millis << std::endl;
+                            if ((millis < 90) || (depth < 2 && millis < 95))
                                 futureSteps = runOneStep(&newstate, depth + 1);
                             else
                                 futureSteps = runAlreadyPlantedBombs(&newstate);
