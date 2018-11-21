a32ed4eb30c4f7aaaa8348af59e1cdc078214047
marton.gorog <marton.gorog@aimotive.com>
2018-11-21 22:08:30 +0100

    FIX!!!

--- src/bboard/bboard.cpp
+++ src/bboard/bboard.cpp
@@ -75,8 +75,10 @@ inline bool SpawnFlameItem(State& s, int x, int y, uint16_t signature, int agent
             if(closestAgent >= 0) {
                 s.agents[closestAgent].woodDemolished += 1.0f - s.relTimeStep / 50.0f;
             }*/
-            if(BMB_ID_KNOWN(agentID))
+            if(agentID < BMB_ID_UNKNOWN)
+            {
                 s.agents[agentID].woodDemolished += 1.0f - s.relTimeStep / 50.0f;
+            }
 
             s.board[y][x] += WOOD_POWFLAG(old); // set the powerup flag
         }
